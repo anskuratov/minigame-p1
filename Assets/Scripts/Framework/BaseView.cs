a67917e66ce5d5d8b8ceac6c0e43a21834010545
Андрей Скуратов <skuratovandry@gmail.com>
2022-05-10 19:41:00 +0400

    Допилил BaseView

--- Assets/Scripts/Framework/BaseView.cs
+++ Assets/Scripts/Framework/BaseView.cs
@@ -4,6 +4,20 @@ namespace P1.Framework
 {
 	public class BaseView : MonoBehaviour
 	{
+		internal void SetScale(Vector3 vector)
+		{
+			transform.localScale = vector;
+		}
+
+		internal void SetPosition(Vector3 vector)
+		{
+			transform.localPosition = vector;
+		}
+
+		internal void SetActive(bool value)
+		{
+			gameObject.SetActive(value);
+		}
 	}
 
 	public abstract class BaseViewController<TView, TInitData> : IInitializable<TInitData>
@@ -22,6 +36,31 @@ namespace P1.Framework
 		public void SetView(TView view)
 		{
 			View = view;
+		}
+
+		protected void SetScale(Vector3 vector)
+		{
+			if (View != null)
+			{
+				View.SetScale(vector);
+			}
+		}
+
+		protected void SetPosition(Vector3 vector)
+		{
+			if (View != null)
+			{
+				View.SetPosition(vector);
+			}
+		}
+
+		protected void SetActive(bool value)
+		{
+			if (View != null
+				&& View.gameObject.activeSelf != value)
+			{
+				View.SetActive(value);
+			}
 		}
 	}
 }
