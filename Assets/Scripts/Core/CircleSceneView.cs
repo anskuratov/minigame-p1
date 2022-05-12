f7a630093a6543b1b9aed0b8991d9f5e6ed39e2c
Андрей Скуратов <skuratovandry@gmail.com>
2022-05-12 23:30:09 +0400

    Добавил коннекшн для кружочков

--- Assets/Scripts/Core/CircleSceneView.cs
+++ Assets/Scripts/Core/CircleSceneView.cs
@@ -1,3 +1,4 @@
+using System;
 using P1.Framework;
 using UnityEngine;
 
@@ -10,6 +11,16 @@ namespace P1.Core
 
 		public SpriteRenderer SpriteRenderer => _spriteRenderer;
 		public LineRenderer LineRenderer => _lineRenderer;
+
+		public event Action OnConnected;
+
+		public void Connect()
+		{
+			OnConnected?.Invoke();
+		}
+
+		// TODO: Это должна быть модель, доступная как из View так и из контроллера
+		public int Number { get; set; }
 	}
 
 	public class CircleSceneViewController : BaseViewController<CircleSceneView, CircleSceneViewController.InitData>
@@ -24,19 +35,26 @@ namespace P1.Core
 			}
 		}
 
+		private int _number;
+		private bool _isConnected;
+
 		protected override void HandleInit(InitData initData)
 		{
-			SetPosition(initData.Circle.Position);
-			SetColor(initData.Circle.Number);
+			_number = initData.Circle.Number;
+			View.Number = initData.Circle.Number;
 
+			SetPosition(initData.Circle.Position);
+			SetColor();
+
+			View.OnConnected += OnConnected;
 			View.OnDragStarted += OnDragStarted;
 			View.OnDragEnded += OnDragEnded;
 			View.OnDragged += OnDragged;
 		}
 
-		private void SetColor(int number)
+		private void SetColor()
 		{
-			if (ColorUtils.TryGetCircleColor(number, out var color))
+			if (ColorUtils.TryGetCircleColor(_number, out var color))
 			{
 				View.SpriteRenderer.color = color;
 				View.LineRenderer.startColor = color;
@@ -46,6 +64,14 @@ namespace P1.Core
 			{
 				View.SpriteRenderer.color = Color.black;
 			}
+		}
+
+		private void OnConnected()
+		{
+			View.OnConnected -= OnConnected;
+			View.OnDragStarted -= OnDragStarted;
+			View.OnDragEnded -= OnDragEnded;
+			View.OnDragged -= OnDragged;
 		}
 
 		private void OnDragStarted()
@@ -61,6 +87,21 @@ namespace P1.Core
 		private void OnDragged(IPointer pointer)
 		{
 			View.LineRenderer.SetPosition(1, pointer.Position);
+
+			if (RaycastUtils.TryRaycast(out var transform))
+			{
+				var circleSceneView = transform.GetComponent<CircleSceneView>();
+
+				if (circleSceneView != null
+					&& View != circleSceneView
+					&& View.Number == circleSceneView.Number)
+				{
+					View.Connect();
+					circleSceneView.Connect();
+
+					View.LineRenderer.SetPosition(1, circleSceneView.transform.localPosition);
+				}
+			}
 		}
 	}
 }
