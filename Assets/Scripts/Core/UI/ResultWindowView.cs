551454cf8810ee9f9f55f44a071751493995c837
Andrei Skuratov <andrei.skuratov@playgendary.com>
2023-03-26 20:54:57 +0500

    Добавил изменение цвета бэкграунда экрана завершения уровня

--- Assets/Scripts/Core/UI/ResultWindowView.cs
+++ Assets/Scripts/Core/UI/ResultWindowView.cs
@@ -9,15 +9,33 @@ namespace P1.Core
 	{
 		[SerializeField] private Button _nextButton;
 		[SerializeField] private Button _retryButton;
+		[SerializeField] private Image _windowBackgroundImage;
 		[SerializeField] private View _rewardContainer;
 		[SerializeField] private TMP_Text _resultText;
 		[SerializeField] private TMP_Text _coinsCountText;
 
+		[Space]
+
+		[Header("Settings")]
+		[SerializeField] private Color _winColor;
+		[SerializeField] private Color _defeatColor;
+
 		public Button NextButton => _nextButton;
 		public Button RetryButton => _retryButton;
 		public View RewardContainer => _rewardContainer;
-		public TMP_Text ResultText => _resultText;
 		public TMP_Text CoinsCountText => _coinsCountText;
+
+		public void SetWin()
+		{
+			_resultText.text = "Well done!";
+			_windowBackgroundImage.color = _winColor;
+		}
+
+		public void SetDefeat()
+		{
+			_resultText.text = "Try again!";
+			_windowBackgroundImage.color = _defeatColor;
+		}
 	}
 
 	public class ResultWindowViewController : ViewController<ResultWindowView>
@@ -53,12 +71,12 @@ namespace P1.Core
 		{
 			if (_gameManager.LevelResult == LevelResult.Win)
 			{
-				View.ResultText.text = "Good job!";
+				View.SetWin();
 				View.CoinsCountText.text = _gameManager.CoinsReward.ToString();
 			}
 			else if (_gameManager.LevelResult == LevelResult.Defeat)
 			{
-				View.ResultText.text = "Try again!";
+				View.SetDefeat();
 			}
 
 			View.RewardContainer.SetActive(_gameManager.LevelResult == LevelResult.Win
